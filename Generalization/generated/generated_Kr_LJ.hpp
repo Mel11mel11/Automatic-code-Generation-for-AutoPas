695fd4a48c2733e0ec1d989ce490f61886d52c83
Melisa Aslan <melisaaslan1138@gmail.com>
2025-11-01 17:16:49 +0100

    test version1

--- Generalization/generated/generated_Kr_LJ.hpp
+++ Generalization/generated/generated_Kr_LJ.hpp
@@ -5,16 +5,16 @@
 #include <cmath>
 
 template <class Particle_T>
-class TestFunctor_Gen : public Functor<Particle_T> {
+class KryptonLJFunctor_Gen : public Functor<Particle_T> {
 public:
-    explicit TestFunctor_Gen(bool newton3 = true)
-        : _newton3(newton3) {}
+    explicit KryptonLJFunctor_Gen(bool newton3 = true, double sigma, double epsilon)
+        : _newton3(newton3), _sigma(sigma), _epsilon(epsilon) {}
 
     bool allowsNewton3() const override { return true; }
     bool usesNewton3()   const override { return _newton3; }
 
     void AoSFunctor(Particle_T& a, Particle_T& b) override {
-
+        // Displacement a -> b (keep the same direction convention as reference)
         const auto& ra = a.getR();
         const auto& rb = b.getR();
         double dx = ra[0] - rb[0];
@@ -28,8 +28,13 @@ public:
         const double r = std::sqrt(r2);
         const double inv_r = 1.0 / r;
 
-        // Fmag = -2.0*r
-        const double Fmag = -2.0*r;
+        // Parameter aliases
+        const double sigma = _sigma;
+        const double epsilon = _epsilon;
+
+        // --- codegen: force magnitude F(r, params) ---
+        // Fmag = -4.0*epsilon*(6.0*std::pow(sigma, 6)/std::pow(r, 7) - 12.0*std::pow(sigma, 12)/std::pow(r, 13))
+        const double Fmag = -4.0*epsilon*(6.0*std::pow(sigma, 6)/std::pow(r, 7) - 12.0*std::pow(sigma, 12)/std::pow(r, 13));
 
         // Vector force: F = Fmag * r̂
         const double fx = Fmag * dx * inv_r;
@@ -44,4 +49,6 @@ public:
 
 private:
     bool _newton3;
+    double _sigma;
+    double _epsilon;
 };
